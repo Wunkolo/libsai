f43eec9da09240689d4dd7dd1e1c5a685d27838d
Wunkolo <Wunkolo@gmail.com>
2016-08-06 22:23:21 -0700

    VFS verification todo notice

--- libsai/sai.cpp
+++ libsai/sai.cpp
@@ -45,6 +45,7 @@ namespace sai
 			// Verify all clusters
 			VFSCluster CurTable;
 			VFSCluster CurCluster;
+			// TODO: Speed htis up!, threaded semaphore
 			for( size_t i = 0; i < ClusterCount; i++ )
 			{
 				if( !(i & 0x1FF) ) // Cluster is a table
