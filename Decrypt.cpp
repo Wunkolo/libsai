3852be4e0e0d51ff2f9808875a3582e76a81bb3f
Wunkolo <Wunkolo@gmail.com>
2017-01-21 11:52:25 -0400

    Add decryption benchmark

--- Decrypt.cpp
+++ Decrypt.cpp
@@ -2,15 +2,33 @@
 Sample code to decrypt any user-created .sai file
 */
 
+#include <stdint.h>
 #include <fstream>
 #include <iostream>
-#include <stdint.h>
+#include <chrono>
+#include <utility>
 #include "libsai/sai.hpp"
 
 const char* const Help =
 "Decrypt user-created .sai files:\n"
 "\tDecrypt.exe (filename) (output)\n"
 "\tWunkolo - Wunkolo@gmail.com";
+
+template<typename TickType = std::chrono::milliseconds>
+struct Benchmark
+{
+	template<typename F, typename ...Args>
+	static typename TickType::rep Run(F func, Args&&... args)
+	{
+		auto StartPoint = std::chrono::system_clock::now();
+
+		func(std::forward<Args>(args)...);
+
+		auto Duration = std::chrono::duration_cast<TickType>(std::chrono::system_clock::now() - StartPoint);
+
+		return Duration.count();
+	}
+};
 
 int main(int argc, char* argv[])
 {
@@ -21,7 +39,7 @@ int main(int argc, char* argv[])
 	}
 
 	sai::ifstreambuf FileIn;
-	FileIn.open(argv[1], std::ios::binary);
+	FileIn.open(argv[1]);
 
 	if( !FileIn.is_open() )
 	{
@@ -38,7 +56,13 @@ int main(int argc, char* argv[])
 		return EXIT_FAILURE;
 	}
 
-	FileOut << &FileIn;
+	std::cout << "File decrypted in:"
+		<< Benchmark<>::Run(
+		[&]() -> void
+	{
+		FileOut << &FileIn;
+	}
+	) << "ms" << std::endl;
 
 	return EXIT_SUCCESS;
 }
