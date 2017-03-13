c5807b41af3cedd8fb66b60e44aa667c74b56011
Wunkolo <Wunkolo@gmail.com>
2017-03-12 17:14:53 -0700

    Add simd speedup for BGRA to RGBA conversion

--- libsai/sai.cpp
+++ libsai/sai.cpp
@@ -3,6 +3,8 @@
 #include <fstream>
 #include <algorithm>
 
+#include <tmmintrin.h>
+
 namespace sai
 {
 /// Internal Structures
@@ -600,12 +602,38 @@ std::tuple<
 
 		Thumbnail->Read(
 			Pixels.get(),
-			Header.Width * Header.Height * sizeof(std::uint32_t)
+			PixelCount * sizeof(std::uint32_t)
 		);
 
 		// BGRA to RGBA
+		std::size_t i = 0;
 
-		for( std::size_t i = 0; i < PixelCount * sizeof(std::uint32_t); i += sizeof(std::uint32_t) )
+		// Simd speedup, four pixels at a time
+		while( i < ((PixelCount * sizeof(std::uint32_t)) & ~0xF) )
+		{
+			const __m128i Swizzle =
+				_mm_set_epi8(
+					15, 12, 13, 14,
+					11, 8, 9, 10,
+					7, 4, 5, 6,
+					3, 0, 1, 2
+				);
+
+			__m128i QuadPixel = _mm_loadu_si128(
+				reinterpret_cast<__m128i*>(&Pixels[i])
+			);
+
+			QuadPixel = _mm_shuffle_epi8(QuadPixel, Swizzle);
+
+			_mm_store_si128(
+				reinterpret_cast<__m128i*>(&Pixels[i]),
+				QuadPixel
+			);
+
+			i += (sizeof(std::uint32_t) * 4);
+		}
+
+		for( ; i < PixelCount * sizeof(std::uint32_t); i += sizeof(std::uint32_t) )
 		{
 			std::swap<std::uint8_t>(Pixels[i], Pixels[i + 2]);
 		}
