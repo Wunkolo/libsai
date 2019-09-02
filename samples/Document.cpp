fd62b1e99f369d22996b27fc488562344fafbd8d
Wunkolo <Wunkolo@gmail.com>
2019-09-02 16:12:02 -0700

    Add document sample

---
+++ samples/Document.cpp
@@ -0,0 +1,54 @@
+#include <cstdint>
+#include <ctime>
+#include <fstream>
+#include <iostream>
+#include <chrono>
+#include <utility>
+#include <sai.hpp>
+
+#include "Benchmark.hpp"
+
+const char* const Help =
+"Show .sai document information:\n"
+"\tDocument (filenames)\n"
+"\tWunkolo - Wunkolo@gmail.com";
+
+int main(int argc, char* argv[])
+{
+	if( argc < 2 )
+	{
+		std::puts(Help);
+		return EXIT_FAILURE;
+	}
+
+	for( std::size_t i = 1; i < std::size_t(argc); ++i)
+	{
+		sai::Document CurDocument(argv[i]);
+
+		if( !CurDocument.IsOpen() )
+		{
+			std::cout << "Error opening file for reading: " << argv[i] << std::endl;
+			return EXIT_FAILURE;
+		}
+
+		const std::tuple<std::uint32_t,std::uint32_t> CanvasSize
+			= CurDocument.GetCanvasSize();
+		std::printf(
+			"Width: %u Height: %u\n",
+			std::get<0>(CanvasSize), std::get<1>(CanvasSize)
+		);
+
+		const auto Bench = Benchmark<std::chrono::nanoseconds>::Run(
+			[&CurDocument]() -> void
+			{
+			}
+		);
+		std::printf(
+			"Iterated VFS of %s in %zu ns\n",
+			argv[i],
+			Bench.count()
+		);
+	}
+
+	return EXIT_SUCCESS;
+}
